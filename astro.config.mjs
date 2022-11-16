1a7fe11b0b1c40431880a12d5d526f33f4268d8c
Verhille <titouan.verhille@epita.fr>
2022-11-16 21:03:27 +0100

    netlify ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [
@@ -21,12 +18,15 @@ export default defineConfig({
     extendDefaultPlugins: true,
   },
   integrations: [tailwind(), mdx(), preact(), image()],
+  output: "server",
+  adapter: netlify(),
 });
 import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
 import image from "@astrojs/image";
+import netlify from "@astrojs/netlify/functions";
 function remarkReadingTime() {
   return function (tree, { data }) {
     const textOnPage = toString(tree);
